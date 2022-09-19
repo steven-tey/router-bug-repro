1b6ad74a6cb6139e12b5730c6bd1743842aaa683
Steven Tey <stevensteel97@gmail.com>
2022-09-18 20:11:57 -0700

    Update [slug].js

--- pages/[slug].js
+++ pages/[slug].js
@@ -2,7 +2,8 @@ import { useRouter } from "next/router";
 
 export default function DynamicRoute({slug}) {
    const router = useRouter()
-  if (router.query.slug === "blocked") {
+   const { slug } = router.query
+  if (slug === "blocked") {
     router.replace("/404", router.asPath)
   }
   
