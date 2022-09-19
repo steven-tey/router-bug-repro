f11ea61517d199940a7e28f99b9ffd91042a191c
Steven Tey <stevensteel97@gmail.com>
2022-09-18 20:11:29 -0700

    Update [slug].js

--- pages/[slug].js
+++ pages/[slug].js
@@ -2,27 +2,11 @@ import { useRouter } from "next/router";
 
 export default function DynamicRoute({slug}) {
    const router = useRouter()
-  if (slug === "blocked") {
+  if (router.query.slug === "blocked") {
     router.replace("/404", router.asPath)
   }
   
   return (
     <h1>{slug}</h1>
     )
-}
-
-export async function getStaticPaths() {
-  return {
-    paths: [],
-    fallback: "blocking",
-  }
-}
-
-export async function getStaticProps(context) {
-  const { slug } = context.params;
-  return {
-    props: {
-      slug
-    }, 
-  }
 }
